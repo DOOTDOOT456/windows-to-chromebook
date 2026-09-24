f76b32eb0cebdbcb3b43d5d7f8e25b43d545c1a4
DOOTDOOT456 <dcsacc456@gmail.com>
2026-09-24 12:13:04 -0400

    Create extract.bat
    Incase school blocks extracting

---
+++ extract.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Forcing extraction of Node.js... please wait...
+
+:: Use Windows' built-in tar command to extract the zip file
+tar -xf "C:\Users\233778\Downloads\node-v26.10.0-win-x64.zip" -C "C:\Users\233778\Downloads"
+
+echo Done! Check your Downloads folder for the unzipped folder.
+pause
